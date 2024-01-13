a5f1cf526405f8c361a78e6412a53d23d5bdcb5d
robykartis <robykarti.s@gmail.com>
2024-01-14 02:30:20 +0700

    Lanjut besok

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Architecture, design, and engineering blog';
-export const HOMEPAGE_ARCTICLE_LIMIT = 7;
+export const HOMEPAGE_ARCTICLE_LIMIT = 6;
 export const ARCTICLE_PER_PAGE = 6;
