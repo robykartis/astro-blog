2fff948afabebbc3253b9f2fd44e1a8b447a4fe9
robykartis <robykarti.s@gmail.com>
2024-01-13 21:11:35 +0700

    Astro Layout

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Architecture, design, and engineering blog';
+export const HOMEPAGE_ARCTICLE_LIMIT = 7;
+export const ARCTICLE_PER_PAGE = 6;
